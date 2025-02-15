a58ab20501adc570b2bc3bda2eca250625edb178
Juan Pablo Paternina <juan.paternina@gmail.com>
2025-02-15 17:49:35 +0000

    [UPDATE] implement TabTwoScreen with styled layout and title display

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -1,3 +1,23 @@
+import Colors from "@/constants/Colors";
+import { Text, View, StyleSheet } from "react-native";
+
 export default function TabTwoScreen() {
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>03</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 200,
+    backgroundColor: Colors.default.background,
+  },
+  title: {
+    fontSize: 126,
+    color: "#BFBFC0",
+  },
+});
